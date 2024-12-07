b5bb764bef9894f03a85160d64651774105bb940
shivam anand <shivam850anand@gmail.com>
2024-12-08 02:23:29 +0530

    ReplyboxHeader addded to codebase

--- src/app/(root)/mail-dashboard/page.tsx
+++ src/app/(root)/mail-dashboard/page.tsx
@@ -1,14 +1,19 @@
 import Inbox from "@/app/_components/_mail-dashboard/inbox";
+import ReplyBox from "@/app/_components/_mail-dashboard/reply-box";
+import ReplyboxHeader from "@/app/_components/_mail-dashboard/replybox-header";
 import LinkAccountbtn from "@/components/linkaccountbtn";
 import { UserButton } from "@clerk/nextjs";
 
 function MaildashBoard() {
   return (
     <div className="f flex flex-1 flex-col gap-4 p-2 pt-0 lg:flex-row">
-      <div className="h-[100vh] flex-1 rounded-xl overflow-y-scroll bg-white md:min-h-min">
-        <Inbox />
+      <div className="h-[100vh] flex-1 overflow-y-scroll rounded-xl bg-white md:min-h-min">
+        {/* <Inbox /> */}
       </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-green-300 md:min-h-min"></div>
+      <div className="h-[100vh] flex-1 overflow-y-scroll rounded-xl bg-white md:min-h-min">
+        <ReplyboxHeader />
+        <ReplyBox />
+      </div>
     </div>
   );
 }
