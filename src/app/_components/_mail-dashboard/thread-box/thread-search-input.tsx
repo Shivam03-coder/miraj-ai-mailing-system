5cc2c451b67e742c28027d853402a242954fa11c
shivam anand <shivam850anand@gmail.com>
2024-12-10 23:55:19 +0530

    added to codebase

--- src/app/_components/_mail-dashboard/thread-box/thread-search-input.tsx
+++ src/app/_components/_mail-dashboard/thread-box/thread-search-input.tsx
@@ -1,9 +1,13 @@
 "use client";
 import { Input } from "@/components/ui/input";
+import useThreads from "@/hooks/use-threads";
 import { Search } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 const ThreadSerachInput = () => {
+  const [SearchValue, setSearchValue] = useState<boolean>();
+  const { isFetching } = useThreads();
+
   return (
     <div className="flex items-center gap-2 rounded bg-paleblue px-3 py-1">
       <Search size={25} />
