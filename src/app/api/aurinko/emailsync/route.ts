0673fde5a17551d334c8b885903d9c85bcd1b906
shivam anand <shivam850anand@gmail.com>
2024-12-10 14:07:39 +0530

    done

--- src/app/api/aurinko/emailsync/route.ts
+++ src/app/api/aurinko/emailsync/route.ts
@@ -1,48 +1,48 @@
-// import { Account } from "@/helpers";
-// import { db } from "@/server/db";
-// import { SyncToDataBase } from "@/utils/database-sync";
-// import { type NextRequest, NextResponse } from "next/server";
+import { Account } from "@/helpers";
+import { db } from "@/server/db";
+import { SyncToDataBase } from "@/utils/database-sync";
+import { type NextRequest, NextResponse } from "next/server";
 
-// export const maxDuration = 300;
+export const maxDuration = 300;
 
-// export const POST = async (req: NextRequest) => {
-//   const body = await req.json();
-//   const { accountId, userId } = body;
-//   if (!accountId || !userId)
-//     return NextResponse.json({ error: "INVALID_REQUEST" }, { status: 400 });
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const { accountId, userId } = body;
+  if (!accountId || !userId)
+    return NextResponse.json({ error: "INVALID_REQUEST" }, { status: 400 });
 
-//   const dbAccount = await db.account.findUnique({
-//     where: {
-//       id: accountId,
-//       userId,
-//     },
-//   });
-//   console.log("🚀 ~ POST ~ dbAccount:", dbAccount);
+  const dbAccount = await db.account.findUnique({
+    where: {
+      id: accountId,
+      userId,
+    },
+  });
+  console.log("🚀 ~ POST ~ dbAccount:", dbAccount);
 
-//   if (!dbAccount)
-//     return NextResponse.json({ error: "ACCOUNT_NOT_FOUND" }, { status: 404 });
+  if (!dbAccount)
+    return NextResponse.json({ error: "ACCOUNT_NOT_FOUND" }, { status: 404 });
 
-//   const account = new Account(dbAccount.token);
-//   const response = await account.primaryEmailSyncFunc();
-//   if (!response)
-//     return NextResponse.json({ error: "FAILED_TO_SYNC" }, { status: 500 });
+  const account = new Account(dbAccount.token);
+  const response = await account.primaryEmailSyncFunc();
+  if (!response)
+    return NextResponse.json({ error: "FAILED_TO_SYNC" }, { status: 500 });
 
-//   const { deltaToken, recordEmails } = response;
+  const { deltaToken, recordEmails } = response;
 
-//   try {
-//     await SyncToDataBase(recordEmails, accountId);
+  try {
+    await SyncToDataBase(recordEmails, accountId);
 
-//     await db.account.update({
-//       where: {
-//         token: dbAccount.token,
-//       },
-//       data: {
-//         nextDeltaToken: deltaToken,
-//       },
-//     });
-//   } catch (error) {
-//     console.log("🚀 ~ POST ~ error:", error);
-//   }
-//   console.log("sync complete", deltaToken);
-//   return NextResponse.json({ success: true, deltaToken }, { status: 200 });
-// };
+    await db.account.update({
+      where: {
+        token: dbAccount.token,
+      },
+      data: {
+        nextDeltaToken: deltaToken,
+      },
+    });
+  } catch (error) {
+    console.log("🚀 ~ POST ~ error:", error);
+  }
+  console.log("sync complete", deltaToken);
+  return NextResponse.json({ success: true, deltaToken }, { status: 200 });
+};
